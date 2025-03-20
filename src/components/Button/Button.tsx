0b3d9cf2cd158261b60fcb6def35d67c87a92d1b
Rayanne Moraes <moraesrayanne@hotmail.com>
2025-03-20 19:46:14 -0300

    feature: added forwardRef in button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,18 +1,22 @@
-import { ButtonHTMLAttributes } from 'react'
+import { ButtonHTMLAttributes, forwardRef } from 'react'
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: string
   variant?: 'primary' | 'outline'
 }
 
-export const Button = ({ children, variant, ...props }: ButtonProps) => {
-  return (
-    <button
-      role="button"
-      {...props}
-      className="bg-blue-600 text-white p-2 w-full font-bold rounded-md"
-    >
-      {children}
-    </button>
-  )
-}
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
+    const { children } = props
+
+    return (
+      <button
+        {...props}
+        ref={ref}
+        role="button"
+        className="bg-blue-600 text-white p-2 w-full font-bold rounded-md"
+      >
+        {children}
+      </button>
+    )
+  }
+)
