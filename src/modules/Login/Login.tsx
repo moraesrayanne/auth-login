6161717ee03fea2edfba698afeaa088be2d19348
Rayanne Moraes <moraesrayanne@hotmail.com>
2025-03-24 17:01:39 -0300

    feat: create form w/ react hook form

--- src/modules/Login/Login.tsx
+++ src/modules/Login/Login.tsx
@@ -1,12 +1,27 @@
-import { LogoHeader, LoginFields, LoginButton, AuthActions } from '@/modules/Login/components'
+import { LogoHeader, AuthActions } from '@/modules/Login/components'
+import { useLoginForm } from '@/modules/Login/hooks/useLoginForm'
+import { Button, Input } from '@/components'
 
 export const Login: React.FC = () => {
+  const { register, handleSubmit, errors } = useLoginForm()
+
+  const onSubmit = (data: any) => {
+    console.log(data)
+  }
+
   return (
     <main className="flex flex-col p-4 h-svh justify-center md:items-center">
-      <div className='flex flex-col gap-6 md:w-[600px]'>
+      <div className="flex flex-col gap-6 md:w-[600px]">
         <LogoHeader />
-        <LoginFields />
-        <LoginButton />
+        <form className="flex flex-col gap-6" onSubmit={handleSubmit(onSubmit)}>
+          <section className="flex flex-col gap-4">
+            <Input placeholder="Enter email" {...register('username')} error={errors.username} />
+            <Input placeholder="Enter password" type="password" {...register('password')} error={errors.password} />
+          </section>
+          <section className="flex flex-col gap-6">
+            <Button type="submit">Sign In</Button>
+          </section>
+        </form>
         <AuthActions />
       </div>
     </main>
