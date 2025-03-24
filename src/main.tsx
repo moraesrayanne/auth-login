844a5d2abf677792b43b09537a36d6277b69008f
Rayanne Moraes <moraesrayanne@hotmail.com>
2025-03-24 15:44:05 -0300

    fix: remove app.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { App } from './App'
+import { Login } from './modules/Login/Login'
 import './index.css'
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Login />
+  </StrictMode>
 )
