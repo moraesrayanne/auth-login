1c680e93695b50b8760dfe51624a443bb50d80da
Rayanne Moraes <moraesrayanne@hotmail.com>
2025-03-24 15:53:10 -0300

    refactor: change variables

--- src/modules/Login/Login.tsx
+++ src/modules/Login/Login.tsx
@@ -1,28 +1,28 @@
 import { Button, Input } from '@/components'
 import { Link } from '@/components/ui/Link'
 
-const Title = () => (
+const LogoHeader = () => (
   <section className="flex justify-center items-center gap-4">
-    <img src="./src/assets/ui_unicorn.png" className="" />
+    <img src="./src/assets/ui_unicorn.png" className="" alt="FinSafe logo" />
     <span className="text-2xl">FinSafe</span>
   </section>
 )
 
-const InputForm = () => (
+const LoginFields = () => (
   <section className="flex flex-col gap-4">
     <Input placeholder="Email or phone number" />
     <Input placeholder="Enter password" type="password" />
   </section>
 )
 
-const ButtonArea = () => (
+const LoginButton = () => (
   <section className="flex flex-col gap-6">
     <Button>Sign In</Button>
   </section>
 )
 
-const Subscription = () => (
-  <section className="flex justify-center">
+const AuthActions = () => (
+  <section className="flex justify-center" aria-labelledby="signup-label">
     <span>Don't have an account?</span>
     <Link href='#'>
       Sign up now
@@ -34,10 +34,10 @@ export const Login: React.FC = () => {
   return (
     <main className="flex flex-col p-4 h-svh justify-center md:items-center">
       <div className='flex flex-col gap-6 md:w-[600px]'>
-        <Title />
-        <InputForm />
-        <ButtonArea />
-        <Subscription />
+        <LogoHeader />
+        <LoginFields />
+        <LoginButton />
+        <AuthActions />
       </div>
     </main>
   )
